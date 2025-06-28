13e5f8514ec748b4e1e60a8e65397a0784857db4
opendotfund <help@openfund.online>
2025-06-28 12:37:51 -0400

    Update SubscriptionModal.tsx

--- components/SubscriptionModal.tsx
+++ components/SubscriptionModal.tsx
@@ -274,7 +274,7 @@ export const SubscriptionModal: React.FC<SubscriptionModalProps> = ({
                   <svg className="h-4 w-4 text-yellow-300 mr-2" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
-                  Unlimited usage potential
+                  Unlimited Community Remixes
                 </div>
               </div>
 
